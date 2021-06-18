2ff56a8a418a2f57f9595a0a0ff9da0ffc8777e6
SpiralSea <86051944+SpiralSea@users.noreply.github.com>
2021-06-18 11:56:15 +1000

    Main.py
    Run this in pycharm, sublime, anything that will run python really, or copy the code I will attatch in the repository.

---
+++ Joke machine.py
@@ -0,0 +1,169 @@
+import random
+import time
+
+for count in range(10):
+
+    input('So, you want to hear a joke? ')
+
+    n = random.randint(0, 10)
+
+    print(' ')
+
+    # anything from here on is just a bank of jokes, do whatever you want with this.
+
+    if n == 1:
+        print('There are three types of people in the world.')
+        print("People who can count, and people who can't")
+
+    if n == 2:
+        print('did you hear about the italian chef who died?')
+        print("he pasta-way")
+
+    if n == 3:
+        print('did you hear about the shoe factory that went up in flames?')
+        print("10,000 soles were lost")
+
+    if n == 4:
+        print('did you hear about the fire at the bakery?')
+        print("the business was toast.")
+
+    if n == 5:
+        print('whats the difference between a rabbit and a plum?')
+        print("they are both purple. except for the rabbit.")
+
+    if n == 6:
+        print('2 windmills were at a wind farm.')
+        print("one asks, 'whats you favourite music genre?'")
+        print("The other one replies, 'Im a big metal fan'")
+
+    if n == 7:
+        print('I like elephants,')
+        print("everything else is irrelephant")
+
+    if n == 8:
+        print('whats red and bad for your teeth?')
+        print("a brick.")
+
+    if n == 9:
+        print('What do you call a fake noodle?')
+        print("an impasta")
+
+    if n == 10:
+        print('did you hear about the claustrophobic astronaut?')
+        print("He just needed some space")
+
+    if n == 0:
+        print('what do you call an alligator with a vest?')
+        print("an in-vest-a-gator")
+
+    print(' ')
+
+    input('Are you  ready for another one? ')
+
+    import random
+
+    a = random.randint(0, 10)
+
+    print(' ')
+
+    if a == 1:
+        print('There are three types of people in the world.')
+        print("People who can count, and people who can't")
+
+    if a == 2:
+        print('did you hear about the italian chef who died?')
+        print("he pasta-way")
+
+    if a == 3:
+        print('did you hear about the shoe factory that went up in flames?')
+        print("10,000 soles were lost")
+
+    if a == 4:
+        print('did you hear about the fire at the bakery?')
+        print("the business was toast.")
+
+    if a == 5:
+        print('whats the difference between a rabbit and a plum?')
+        print("they are both purple. except for the rabbit.")
+
+    if a == 6:
+        print('2 windmills were at a wind farm.')
+        print("one asks, 'whats you favourite music genre?'")
+        print("The other one replies, 'Im a big metal fan'")
+
+    if a == 7:
+        print('I like elephants,')
+        print("everything else is irrelephant")
+
+    if a == 8:
+        print('whats red and bad for your teeth?')
+        print("a brick.")
+
+    if a == 9:
+        print('What do you call a fake noodle?')
+        print("an impasta")
+
+    if (a == 10):
+        print('did you hear about the claustrophobic astronaut?')
+        print("He just needed some space")
+
+    if (a == 0):
+        print('what do you call an alligator with a vest?')
+        print("an in-vest-a-gator")
+
+    print(' ')
+
+    input('Are you  ready for another one? ')
+
+    import random
+
+    b = random.randint(0, 10)
+
+    print(' ')
+
+    if (b == 1):
+        print('There are three types of people in the world.')
+        print("People who can count, and people who can't")
+
+    if (b == 2):
+        print('did you hear about the italian chef who died?')
+        print("he pasta-way")
+
+    if (b == 3):
+        print('did you hear about the shoe factory that went up in flames?')
+        print("10,000 soles were lost")
+
+    if (b == 4):
+        print('did you hear about the fire at the bakery?')
+        print("the business was toast.")
+
+    if (b == 5):
+        print('whats the difference between a rabbit and a plum?')
+        print("they are both purple. except for the rabbit.")
+
+    if (b == 6):
+        print('2 windmills were at a wind farm.')
+        print("one asks, 'whats you favourite music genre?'")
+        print("The other one replies, 'Im a big metal fan'")
+
+    if (b == 7):
+        print('I like elephants,')
+        print("everything else is irrelephant")
+
+    if (b == 8):
+        print('whats red and bad for your teeth?')
+        print("a brick.")
+
+    if (b == 9):
+        print('What do you call a fake noodle?')
+        print("an impasta")
+
+    if (b == 10):
+        print('did you hear about the claustrophobic astronaut?')
+        print("He just needed some space")
+
+    if (b == 0):
+        print('what do you call an alligator with a vest?')
+        print("an in-vest-a-gator")
+
+time.sleep(10)
